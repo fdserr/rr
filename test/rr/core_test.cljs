f18b8915abea29131e810818ff388aa3e185d86b
François De Serres <francois.deserres@gmail.com>
2017-09-29 19:40:23 -0500

    Revert "Initial 1.0.0"
    This reverts commit 303b1545bf160e5292fcc86cd19266a5ed5f65b9.

---
+++ test/rr/core_test.cljs
@@ -0,0 +1,6 @@
+(ns rr.core-test
+ (:require [clojure.spec :as spec]
+           [clojure.spec.test :as test]
+           ; [clojure.spec.gen :as gen]
+           [rr.core :as rr]
+           [rr.example :as e]))
